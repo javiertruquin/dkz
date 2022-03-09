a5e1a09c37c7ec6fbc3b7954878fc9b5b316f86b
javier Truquin <javiertruquin@gmail.com>
2022-03-09 19:31:19 -0300

    empezando con css

---
+++ src/components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Banner() {
+     return (
+        <div className="">
+            <div className="bg-hero"></div>
+        </div>
+    );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Banner from "../components/Banner";
+
+
+
+export default function Home() {
+
+    return (
+        <div>
+            <Banner />
+        </div>
+    );
+}
