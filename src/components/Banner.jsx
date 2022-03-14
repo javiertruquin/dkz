c7b86dc41cf60c35ad5b644a3c9d092ddee8ec38
javier Truquin <javiertruquin@gmail.com>
2022-03-14 20:08:22 -0300

    empezando

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 export default function Banner() {
-     return (
-        <div className="">
-            <div className="bg-hero"></div>
+    return (
+        <div className="fondo-banner">
+            <div className="  bg-abajo">
+                <div className="bg-abajo">
+                    <div className="card-position">
+                        <card className="card-home"></card>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
