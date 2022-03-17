ab601f429fbc94754a7fad769f1a486083c20f7f
¨mago¨ <martingramajo08@gmail.com>
2022-03-16 22:27:58 -0300

    agrego los componentes al home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Cartas from "../components/Cartas";
 
 
 
@@ -8,7 +9,7 @@ export default function Home() {
     return (
         <div>
             <Banner />
-
+            <Cartas />
         </div>
     );
 }
