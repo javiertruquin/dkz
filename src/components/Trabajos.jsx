cad1e4c8d3dfb4b7abbff3bc2dd3cdde04243de8
¨mago¨ <martingramajo08@gmail.com>
2022-03-16 23:48:08 -0300

    agrego Trabajos

--- src/components/Trabajos.jsx
+++ src/components/Trabajos.jsx
@@ -1,17 +1,13 @@
-import { Button } from "react-bootstrap";
-
 export default function Trabajos() {
   return (
-    <div className="color-azul-fondo d-flex justify-content-center mt-5">
-      <div className="peso-regular text-white">
+    <div className="color-azul-oscuro-fondo d-flex justify-content-center mt-5">
+      <div className="peso-italic text-white">
         Mirá algunos de nuestros trabajos <br />
         y descubrí lo que somos capaces de <br />
         hacer con tu marca.
         <div>
-          <div className="d-flex justify-content-center mt-4 boton-morado border-0 borde-boton-morado">
-            <Button className="boton-morado border-0">
+          <div className="d-flex justify-content-center mt-4 boton-morado border-0 borde-boton-morado mb-5" >
               Contactanos
-            </Button>
           </div>
         </div>
       </div>
