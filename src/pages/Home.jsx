f1243338881fe3d0da662535fbbdef6abcf9a17d
¨mago¨ <martingramajo08@gmail.com>
2022-03-16 23:50:17 -0300

    los agregos en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Banner from "../components/Banner";
-import Banner2 from "../components/Banner2";
-import NavbarReact from "../components/NavbarReact";
+import Banner3 from "../components/Banner3";
 import Cartas from "../components/Cartas";
+import Footer from "../components/Footer";
+import Frase from "../components/Frase";
+import Iconos from "../components/Iconos";
+import NavbarReact from "../components/NavbarReact";
 
 
 
@@ -12,9 +15,14 @@ export default function Home() {
         <div>
             <NavbarReact />
             <Banner />
-            {/* <Banner2 /> */}
+
+
             
             <Cartas />
+            <Banner3 />
+            <Frase />
+            <Iconos />
+            <Footer />
         </div>
     );
 }
