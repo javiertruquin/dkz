1d5bd82e520d26b327cb1bd79ea72ad1329a5742
javier Truquin <javiertruquin@gmail.com>
2022-03-16 22:31:08 -0300

    ahora si corregi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Banner from "../components/Banner";
-<<<<<<< HEAD
 import Banner2 from "../components/Banner2";
 import NavbarReact from "../components/NavbarReact";
-=======
 import Cartas from "../components/Cartas";
->>>>>>> 901967355f1c6adf40c97dc93bac659f80997e42
 
 
 
@@ -15,12 +12,9 @@ export default function Home() {
         <div>
             <NavbarReact />
             <Banner />
-<<<<<<< HEAD
             {/* <Banner2 /> */}
             
-=======
             <Cartas />
->>>>>>> 901967355f1c6adf40c97dc93bac659f80997e42
         </div>
     );
 }
