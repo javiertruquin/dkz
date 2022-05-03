445b5cf3335c77182821838fe9315dfd6c9db258
¨mago¨ <martingramajo08@gmail.com>
2022-05-03 03:52:56 -0300

    frase agrego letra y centrado

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Frase() {
   return (
-    <div className="color-azul-oscuro-fondo d-flex justify-content-center">
-      <div className="">
-        <div className="efecto-blur-card my-5 me-4">
-          <div className=" my-5 mx-4">
+    <div className="color-azul-oscuro-fondo mx-auto">
+      <div className="d-flex justify-content-center">
+        <div className="efecto-blur-card my-5">
+          <div className="my-5 mx-5">
             <p className="peso-italic text-white text-center espaciado">
               “Aquellos que están lo suficientemente <br />
               locos para creer que pueden <br />
