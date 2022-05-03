780b3ce0bc313214d3781dc3478e795eed4c05d7
¨mago¨ <martingramajo08@gmail.com>
2022-05-03 03:52:35 -0300

    agrego justify-content-end en el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,19 +3,26 @@ import { Image } from "react-bootstrap";
 
 export default function Footer() {
   return (
-    <div className="color-azul-oscuro-fondo d-flex justify-content-around text-white">
-          <div className="text-white mt-4">
+    <>
+      <div className="color-azul-oscuro-fondo d-flex justify-content-between text-white container">
+        <div className="text-white mt-4">
           <Image
-                    className=""
-                    src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/logo-dkz_1_sntx9w.png"
-                    fluid
-                    alt="logo"
-                />
-          </div>
-          <div className="text-white mt-4 mb-4 tamaño-chico">
-              <span>Copyright © 2022</span> <br/>
-              <span>Todos los derechos reservados.</span>
-          </div>
-    </div>
+            className=""
+            src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/logo-dkz_1_sntx9w.png"
+            fluid
+            alt="logo"
+          />
+        </div>
+        <div className="text-white mt-4 mb-4 tamaño-chico">
+          <span className="d-flex justify-content-end">Copyright © 2022</span>
+          <span className="d-flex justify-content-end my-1">
+            Todos los derechos reservados.
+          </span>
+          <span className="d-flex justify-content-end my-1">
+          Product by DKZ studio
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
