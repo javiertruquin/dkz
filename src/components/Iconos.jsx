28cdf7f9775d521630f3c3949e66524304abfe3c
¨mago¨ <martingramajo08@gmail.com>
2022-05-03 03:23:51 -0300

    agregado los iconos nuevos

--- src/components/Iconos.jsx
+++ src/components/Iconos.jsx
@@ -1,39 +1,61 @@
 import React from "react";
 import { Image } from "react-bootstrap";
-import wsp from "../assets/icons/wsp.svg"
+import wsp from "../assets/icons/wsp.svg";
+import face from "../assets/icons/face.svg";
+import linken from "../assets/icons/linken.svg";
+import ig from "../assets/icons/ig.svg";
 import Contacto from "./Contacto";
 
-
 export default function Iconos() {
-    return (
-        <div className="color-azul-oscuro-fondo color-violeta">
-            <div className="text-center">
-                <Image
-                    className="iconos mx-2 mt-4 mb-4"
-                    src={wsp}
-                    fluid
-                    alt="DKZ"
-                />
-                <Image
-                    className="iconos mx-2 mt-4 mb-4"
-                    src={wsp}
-                    fluid
-                    alt="DKZ"
-                />
-                <Image
-                    className="iconos mx-2 mt-4 mb-4"
-                    src={wsp}
-                    fluid
-                    alt="DKZ"
-                />
-                <Image
-                    className="iconos mx-2 mt-4 mb-4"
-                    src={wsp}
-                    fluid
-                    alt="DKZ"
-                />
-            </div>
-            <Contacto />
-        </div>
-    );
+  return (
+    <div className="color-azul-oscuro-fondo color-violeta container">
+      <div className="text-center">
+        <a href="https://www.google.com.ar/" target="_blank" rel="noreferrer">
+          <Image
+            className="iconos mx-2 mt-4 mb-4"
+            src={wsp}
+            fluid
+            alt="icono-wsp"
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/agenciadkz"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            className="iconos mx-2 mt-4 mb-4"
+            src={face}
+            fluid
+            alt="icono-facebook"
+          />
+        </a>
+        <a
+          href="https://www.instagram.com/dkzstudio/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            className="iconos mx-2 mt-4 mb-4"
+            src={ig}
+            fluid
+            alt="icono-instagram"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/dkzstudio/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Image
+            className="iconos mx-2 mt-4 mb-4"
+            src={linken}
+            fluid
+            alt="icono-linken"
+          />
+        </a>
+      </div>
+      <Contacto />
+    </div>
+  );
 }
