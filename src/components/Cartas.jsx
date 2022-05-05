59a6c6016124f74ed1ae4bd76fd998e1c5623487
javier Truquin <javiertruquin@gmail.com>
2022-05-05 00:09:35 -0300

    pequeños cambios para mejor funcionamiento, y agregue fade

--- src/components/Cartas.jsx
+++ src/components/Cartas.jsx
@@ -1,53 +1,52 @@
 import Carta from "./Carta";
 import Trabajos from "./Trabajos";
+import { Bounce } from "react-reveal";
 
 export default function Cartas() {
-  const imagenes = [
-    {
-      image:
-        "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_Ro38zH1CRUY_o30hxa.png",
-      title: "Marketing digital",
-    },
-    {
-      image:
-        "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_sqJ4tLBiurw_yeyos1.png",
-      title: "Eventos",
-    },
-    {
-      image:
-        "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_4D-Q5INzhZE_iyo0vq.png",
-      title: "Producción audiovisual",
-    },
-    {
-      image:
-        "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_8qEB0fTe9Vw_yxqr4y.png",
-      title: "Sitio web",
-    },
-    {
-      image:
-        "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_m2TU2gfqSeE_gmwapk.png",
-      title: "Consultoría",
-    },
-    {
-      image:
-        "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_BcR77nBURV8_miaada.png",
-      title: "Prensa",
-    },
-  ];
+    const imagenes = [
+        {
+            image: "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_Ro38zH1CRUY_o30hxa.png",
+            title: "Marketing digital",
+        },
+        {
+            image: "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_sqJ4tLBiurw_yeyos1.png",
+            title: "Eventos",
+        },
+        {
+            image: "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_4D-Q5INzhZE_iyo0vq.png",
+            title: "Producción audiovisual",
+        },
+        {
+            image: "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_8qEB0fTe9Vw_yxqr4y.png",
+            title: "Sitio web",
+        },
+        {
+            image: "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_m2TU2gfqSeE_gmwapk.png",
+            title: "Consultoría",
+        },
+        {
+            image: "https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_BcR77nBURV8_miaada.png",
+            title: "Prensa",
+        },
+    ];
 
-  return (
-    <div className="color-azul-oscuro-fondo">
-      <div>
-        <div className="row">
-          <h1 className="color-verde text-center my-4"> Digital solutions</h1>
-          <div className="d-flex flex-wrap justify-content-around">
-            {imagenes.map((imagen, id) => (
-              <Carta key={id} data={imagen} />
-            ))}
-          </div>
+    return (
+        <div className="color-azul-oscuro-fondo">
+            <div>
+                <div className="">
+                    <h1 className="color-verde text-center py-4">
+                        {" "}
+                        Digital solutions
+                    </h1>
+
+                    <div className="d-flex flex-wrap justify-content-around">
+                        {imagenes.map((imagen, id) => (
+                            <Carta key={id} data={imagen} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <Trabajos />
         </div>
-      </div>
-      <Trabajos />
-    </div>
-  );
+    );
 }
