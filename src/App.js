b174364f74a2f898e3f8afeb847968dc2183ddee
¨mago¨ <martingramajo08@gmail.com>
2022-05-16 18:32:11 -0300

    agrego las rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import AudioVisuales from "./pages/AudioVisuales";
 // import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-    return <Home />;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Audiovisual" element={<AudioVisuales />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default App;
