472b554e396ce3546776f7826d416d5d948a386e
javier Truquin <javiertruquin@gmail.com>
2022-05-17 19:33:09 -0300

    cards trabajos

---
+++ src/components/Trabajos/Clientes.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Clientes() {
+    return <div>Clientes</div>;
+}
--- src/pages/Trabajos.jsx
+++ src/pages/Trabajos.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import Footer from "../components/Footer";
 import Iconos from "../components/Iconos";
 import BannerT from "../components/Trabajos/BannerT";
+import CTrabajos from "../components/Trabajos/CTrabajos";
+import Clientes from "../components/Trabajos/Clientes";
 import NavbarReact from "../components/NavbarReact";
 
-export default function Home() {
+export default function Trabajos() {
 
     return (
         <div className="color-azul-oscuro-fondo">
             <NavbarReact />
             <BannerT />
+            <CTrabajos />
+            <CTrabajos />
+            <CTrabajos />
+            <Clientes />
             <Iconos />
             <Footer />
         </div>
