ede3b60c6f1142d87184667aec4f03c445e80705
¨mago¨ <martingramajo08@gmail.com>
2022-05-17 18:09:23 -0300

    agrego page conocenos

--- src/components/AudioVisuales/Contacto.jsx
+++ src/components/AudioVisuales/Contacto.jsx
@@ -3,13 +3,7 @@ import Tada from "react-reveal/Tada";
 
 export default function Contacto() {
   return (
-    <div className="pt-5">
-      <div>
-        <h1 className="tamaño-mas-grande peso-bold-italic text-white text-center ">
-          Tu proyecto <br />
-          en buenas manos
-        </h1>
-      </div>
+    <div className="pt-3">
       <Tada>
         <div className="d-flex justify-content-center ">
           <div className="mt-4 boton-morado-visuales border-0 borde-boton-morado">
