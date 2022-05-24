ce1457b12744296e7822d89d8676a1618259cd85
¨mago¨ <martingramajo08@gmail.com>
2022-05-24 17:47:53 -0300

    agrego css para fixed footer

--- src/pages/SitiosWeb.jsx
+++ src/pages/SitiosWeb.jsx
@@ -3,14 +3,21 @@ import NavbarReact from "../components/NavbarReact";
 import SitioConstruccion from "../components/SitioConstruccion";
 import Iconos from "../components/Iconos";
 import Footer from "../components/Footer";
+import SitiosWebsEscritorio from "../components/Escritorio/SitiosWebs/SitiosWebsEscritorio";
+
 
 export default function SitiosWeb() {
-    return (
-        <div className="color-azul-oscuro-fondo">
-            <NavbarReact />
-            <SitioConstruccion />
-            <Iconos />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <div className="color-azul-oscuro-fondo fixed d-sm-none d-block">
+        <NavbarReact />
+        <SitioConstruccion />
+        <Iconos />
+        <Footer />
+      </div>
+      <div className="d-none d-sm-block">
+        <SitiosWebsEscritorio />
+      </div>
+    </div>
+  );
 }
