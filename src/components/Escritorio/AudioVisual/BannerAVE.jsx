95accd3242a8a1e32b0a5ec206fe77022fe8f470
¨mago¨ <martingramajo08@gmail.com>
2022-05-27 23:36:41 -0300

    agrego banner con efecto en audiovisual

--- src/components/Escritorio/AudioVisual/BannerAVE.jsx
+++ src/components/Escritorio/AudioVisual/BannerAVE.jsx
@@ -6,7 +6,7 @@ export default function BannerAVE() {
       <div className="pb-5 color-azul-oscuro-fondo">
         <div className="fondo-banner-av-escritorio">
           <div className="card-position">
-            <div className="banner-pie-av-escritorio  d-flex justify-content-center gradient container-fluid">
+            <div className="banner-pie-av-escritorio  d-flex justify-content-center container-fluid">
               <div className="text-white ">
                 <h1 className=" peso-bold-italic my-2">
                   {" "}
