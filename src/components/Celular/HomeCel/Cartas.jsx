eced6453c8d34d1fd9ad995e6e8371da8e47b774
¨mago¨ <martingramajo08@gmail.com>
2022-05-29 17:06:52 -0300

    Cartas en celular arreglado el linkeo

--- src/components/Celular/HomeCel/Cartas.jsx
+++ src/components/Celular/HomeCel/Cartas.jsx
@@ -1,170 +1,155 @@
 import RubberBand from "react-reveal/RubberBand";
-import { Card } from "react-bootstrap";
+import { Card, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Trabajos from "./Trabajos";
 
 export default function Cartas() {
-    return (
-        <div className="color-azul-oscuro-fondo">
-            <div>
-                <div>
-                    <h1 className="color-verde text-center my-4 peso-bold-italic tamaño-mas-grande mas-espaciado">
-                        {" "}
-                        Digital solutions
-                    </h1>
-                    <div className="d-flex flex-wrap justify-content-around">
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_Ro38zH1CRUY_o30hxa.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className="mb-0 text-center">
-                                        <Link
-                                            className="sin-sub peso-bold-italic tamaño-grande color-violeta"
-                                            to="/MarketingDigital"
-                                        >
-                                            Marketing digital
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_sqJ4tLBiurw_yeyos1.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className=" mb-0 text-center">
-                                        <Link
-                                            className="sin-sub peso-bold-italic tamaño-grande color-violeta "
-                                            to="/Eventos"
-                                        >
-                                            Eventos
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_4D-Q5INzhZE_iyo0vq.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className=" mb-0 text-center">
-                                        <Link
-                                            className="peso-bold-italic tamaño-grande color-violeta sin-sub"
-                                            to="/Audiovisual"
-                                        >
-                                            Producción Audiovisual
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_8qEB0fTe9Vw_yxqr4y.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className=" mb-0 text-center">
-                                        <Link
-                                            className="sin-sub peso-bold-italic tamaño-grande color-violeta"
-                                            to="/SitiosWeb"
-                                        >
-                                            Sitios Web
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_m2TU2gfqSeE_gmwapk.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className=" mb-0 text-center">
-                                        <Link
-                                            className="sin-sub peso-bold-italic tamaño-grande color-violeta"
-                                            to="/Consultoria"
-                                        >
-                                            Consultoria
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_BcR77nBURV8_miaada.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className=" mb-0 text-center">
-                                        <Link
-                                            className="sin-sub peso-bold-italic tamaño-grande color-violeta"
-                                            to="/Prensa"
-                                        >
-                                            Prensa
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-                        <RubberBand>
-                            <Card className="border-card color-blanco my-4 card-digital">
-                                <div className="d-flex justify-content-center">
-                                    <Card.Img
-                                        className="imagen-card"
-                                        src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_m2TU2gfqSeE_gmwapk.png"
-                                        alt="iconos solución digital"
-                                    />
-                                </div>
-                                <Card.Body className="d-flex justify-content-center align-items-center">
-                                    <Card.Title className=" mb-0 text-center">
-                                        <Link
-                                            className="sin-sub peso-bold-italic tamaño-grande color-violeta"
-                                            to="/MarketingContenidos"
-                                        >
-                                            Marketing de contenidos
-                                        </Link>
-                                    </Card.Title>
-                                </Card.Body>
-                            </Card>
-                        </RubberBand>
-                    </div>
-                </div>
-            </div>
-            <Trabajos />
+  return (
+    <div className="color-azul-oscuro-fondo">
+      <div>
+        <div>
+          <h1 className="color-verde text-center my-4 peso-bold-italic tamaño-mas-grande mas-espaciado">
+            {" "}
+            Digital solutions
+          </h1>
+          <div className="d-flex flex-wrap justify-content-around">
+            {/* Card #1 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/MarketingDigital">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_Ro38zH1CRUY_o30hxa.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className="mb-0 text-center  peso-bold-italic tamaño-grande color-violeta">
+                      Marketing digital
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+            {/* Card #2 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/Eventos">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_sqJ4tLBiurw_yeyos1.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className=" mb-0 text-center  peso-bold-italic tamaño-grande color-violeta">
+                      Eventos
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+            {/* Card #3 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/Audiovisual">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290485/Home/unsplash_4D-Q5INzhZE_iyo0vq.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className=" mb-0 text-center peso-bold-italic tamaño-grande color-violeta">
+                      Producción Audiovisual
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+            {/* Card #4 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/SitiosWeb">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_8qEB0fTe9Vw_yxqr4y.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className=" mb-0 text-center peso-bold-italic tamaño-grande color-violeta">
+                      Sitios Web
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+            {/* Card #5 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/Consultoria">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_m2TU2gfqSeE_gmwapk.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className=" mb-0 text-center peso-bold-italic tamaño-grande color-violeta">
+                      Consultoria
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+            {/* Card #6 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/Prensa">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_BcR77nBURV8_miaada.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className=" mb-0 text-center peso-bold-italic tamaño-grande color-violeta">
+                      Prensa
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+            {/* Card #7 */}
+            <RubberBand>
+              <Nav.Link as={Link} to="/MarketingContenidos">
+                <Card className="border-card color-blanco my-4 card-digital">
+                  <div className="d-flex justify-content-center">
+                    <Card.Img
+                      className="imagen-card"
+                      src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1647290484/Home/unsplash_m2TU2gfqSeE_gmwapk.png"
+                      alt="iconos solución digital"
+                    />
+                  </div>
+                  <Card.Body className="d-flex justify-content-center align-items-center">
+                    <Card.Title className=" mb-0 text-center peso-bold-italic tamaño-grande color-violeta">
+                      Marketing de contenidos
+                    </Card.Title>
+                  </Card.Body>
+                </Card>
+              </Nav.Link>
+            </RubberBand>
+          </div>
         </div>
-    );
+      </div>
+      <Trabajos />
+    </div>
+  );
 }
