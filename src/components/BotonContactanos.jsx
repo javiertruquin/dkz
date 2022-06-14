47debf24688d3eb6486a44ec7af4730960c471f9
javier Truquin <javiertruquin@gmail.com>
2022-06-13 23:01:25 -0300

    comentado el efecto tada del boton contactanos provisoriamente

--- src/components/BotonContactanos.jsx
+++ src/components/BotonContactanos.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Tada from "react-reveal/Tada";
+// import Tada from "react-reveal/Tada";
 import { Link } from "react-router-dom";
 
 export default function BotonContactanos() {
     return (
-        <Tada>
+        // <Tada>
             <div className="d-flex justify-content-center">
                 <div className="boton-verde">
                     <div className="d-none d-sm-block">
@@ -29,6 +29,6 @@ export default function BotonContactanos() {
                     </div>
                 </div>
             </div>
-        </Tada>
+        // </Tada>
     );
 }
