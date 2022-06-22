3c429ea933d362335012bb552820ec42ffd6fef7
¨mago¨ <martingramajo08@gmail.com>
2022-06-22 15:13:53 -0300

    muevo el contenido de banner consultoria

--- src/components/Escritorio/Consultoria/BannerConsultaria.jsx
+++ src/components/Escritorio/Consultoria/BannerConsultaria.jsx
@@ -1,5 +1,3 @@
-import BotonContactanos from "../../BotonContactanos";
-
 export default function BannerConsultaria() {
   return (
     <div>
@@ -16,39 +14,20 @@ export default function BannerConsultaria() {
             </p>
             <p className="tamaño-medio  peso-italic text-white text-center espaciado texto-sombra">
               Nos enfocamos en el requerimiento del cliente, brindando
-               soluciones alternativas y eficaces, <br /> que ayuden a consolidar
+              soluciones alternativas y eficaces, <br /> que ayuden a consolidar
               el esquema de trabajo y los objetivos internos de una compañía.
             </p>
-            <div className="d-flex justify-content-center">
-              <ul className="tamaño-medio peso-italic text-white espaciado texto-sombra">
-                <li>Consultoría y asesoramiento estratégico</li>
-                <li>Consultoría y asesoramiento en marketing y comercialización</li>
-                <li>Consultoría y asesoramiento económico - financiero</li>
-                <li>Consultoría y asesoramiento en organización y recursos humanos.</li>
-              </ul>
-            </div>
           </div>
           <div className="d-none d-lg-block">
             <p className="tamaño-mas-grande peso-bold-italic text-white text-center espaciado texto-sombra">
               CONSULTORÍA
             </p>
             <p className="tamaño-grande peso-italic text-white text-center espaciado texto-sombra">
-            Nos enfocamos en el requerimiento del cliente, brindando
-               soluciones alternativas y eficaces, <br /> que ayuden a consolidar
+              Nos enfocamos en el requerimiento del cliente, brindando
+              soluciones alternativas y eficaces, <br /> que ayuden a consolidar
               el esquema de trabajo y los objetivos internos de una compañía.
             </p>
-            
-            <div className="d-flex justify-content-center ">
-              <ul className="tamaño-grande peso-italic text-white espaciado texto-sombra">
-                <li>Consultoría y asesoramiento estratégico</li>
-                <li>Consultoría y asesoramiento en marketing y comercialización</li>
-                <li>Consultoría y asesoramiento económico - financiero</li>
-                <li>Consultoría y asesoramiento en organización y recursos humanos.</li>
-              </ul>
-            </div>
-            
           </div>
-          <BotonContactanos />
         </div>
       </div>
     </div>
