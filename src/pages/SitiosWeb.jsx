829322cf11869a009d1126a72fdd3592affa87f7
¨mago¨ <martingramajo08@gmail.com>
2022-06-22 15:15:06 -0300

    en sitio webs pongo el contenido de pagina en construccion

--- src/pages/SitiosWeb.jsx
+++ src/pages/SitiosWeb.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import BannerSitiosW from "../components/Celular/SitiosWeb/BannerSitiosW";
-import SitiosWebsEscritorio from "../components/Escritorio/SitiosWebs/SitiosWebsEscritorio";
+// import BannerSitiosW from "../components/Celular/SitiosWeb/BannerSitiosW";
+// import SitiosWebsEscritorio from "../components/Escritorio/SitiosWebs/SitiosWebsEscritorio";
+import SitioConstruccion from "../components/SitioConstruccion";
 
 
 export default function SitiosWeb() {
     return (
         <div>
             <div className="color-azul-oscuro-fondo  d-sm-none d-block">
-                <BannerSitiosW />
+                <SitioConstruccion />
             </div>
             <div className="color-azul-oscuro-fondo d-none d-sm-block">
-                <SitiosWebsEscritorio />
+                <SitioConstruccion />
             </div>
         </div>
     );
