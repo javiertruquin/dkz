6cf34edbdbc2ba638f681a7214fcf1c0e9f3860f
¨mago¨ <martingramajo08@gmail.com>
2022-06-22 15:15:20 -0300

    en happyending corrijo el footer q se levantaba

--- src/pages/HappyEnding.jsx
+++ src/pages/HappyEnding.jsx
@@ -1,12 +1,10 @@
-import React from "react";
-import EventosEscritorio from "../components/Escritorio/Eventos/EventosEscritorio";
 import BotonContactanos from "../components/BotonContactanos";
 import HappyEndingCards from "../components/Celular/HappyEnding/HappyEndingCards";
 import HappyEndingCardsEscritorio from "../components/Escritorio/HappyEnding/HappyEndingCardsEscritorio";
 
 export default function HappyEnding() {
     return (
-        <div>
+        <div className="d-flex flex-column" style={{ minHeight: "72vh" }}>
             <div className="color-azul-oscuro-fondo fixed d-sm-none d-block">
                 <HappyEndingCards />
                 <BotonContactanos />
