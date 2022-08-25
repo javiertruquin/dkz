22147255296eb3e0449b80a2f2da911d01c6c3dc
¨mago¨ <martingramajo08@gmail.com>
2022-08-24 21:31:53 -0300

    social media

--- src/components/Escritorio/SocialMedia/BannerSocialM.jsx
+++ src/components/Escritorio/SocialMedia/BannerSocialM.jsx
@@ -15,7 +15,7 @@ export default function BannerSocialM() {
           <p className="tamaño-grande peso-bold-italic text-white text-center espaciado texto-sombra">
             SOCIAL MEDIA
           </p>
-          <p className="tamaño-medio  peso-italic text-white text-center espaciado texto-sombra px-2">
+          <p className="tamaño-medio peso-italic text-white text-center espaciado texto-sombra px-2">
           Desarrollamos estrategias personalizadas para tu empresa. Ponemos creatividad y compromiso  a la hora de ejecutar proyectos digitales, basándonos en una marcada trayectoria a la hora de generar contenidos.  Desarrollamos campañas y acciones específicas a través de las plataformas y canales de redes sociales  de acuerdo con la estrategia global de la empresa. Una estrategia social eficaz puede ayudar a hacer crecer tu negocio y crear un compromiso con el público. Creando estrategias, creando HISTORIAS.  
           </p>
         </div>
