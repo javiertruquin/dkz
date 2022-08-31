5c71483613e139d701656f431027cb7c052f6f97
¨mago¨ <martingramajo08@gmail.com>
2022-08-31 16:29:43 -0300

    texto en el banner arreglado

--- src/components/Escritorio/HomeEscritorio/BannerEscritorio.jsx
+++ src/components/Escritorio/HomeEscritorio/BannerEscritorio.jsx
@@ -2,29 +2,25 @@ import { Image } from "react-bootstrap";
 import BotonContactanos from "../../BotonContactanos";
 
 export default function BannerEscritorio() {
-    return (
-        <>
-            <div className="color-azul-oscuro-fondo">
-                <Image
-                    className="banner-escritorio"
-                    src="https://res.cloudinary.com/dlzcbrqax/image/upload/v1661383015/banners/bannersocialmedia_gcqdlo.jpg"
-                    alt="Banner-escritorio"
-                    fluid
-                />
-                <div className="banner-posi-escritorio">
-                    <div className="d-lg-none d-block">
-                        <p className="tamaño-grande peso-bold-italic text-white text-center espaciado texto-sombra">
-                            LLEVA TUS IDEAS A UN LUGAR QUE NUNCA IMAGINASTE
-                        </p>
-                    </div>
-                    <div className="d-none d-lg-block">
-                        <p className="tamaño-mas-grande peso-bold-italic text-white text-center espaciado texto-sombra">
-                            LLEVA TUS IDEAS A UN LUGAR QUE NUNCA IMAGINASTE
-                        </p>
-                    </div>
-                    <BotonContactanos />
-                </div>
+  return (
+    <div>
+      <div className="color-azul-oscuro-fondo">
+        <div className="fondo-banner">
+          <div className="centrado">
+            <div className="d-lg-none d-block">
+              <p className="tamaño-grande peso-bold-italic text-white text-center espaciado texto-sombra">
+                LLEVA TUS IDEAS A UN LUGAR QUE NUNCA IMAGINASTE
+              </p>
             </div>
-        </>
-    );
+            <div className="d-none d-lg-block ">
+              <p className="tamaño-mas-grande peso-bold-italic text-white text-center espaciado texto-sombra">
+                LLEVA TUS IDEAS A UN LUGAR QUE NUNCA IMAGINASTE
+              </p>
+            </div>
+            <BotonContactanos />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
