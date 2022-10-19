fc43d46a57bcd81a35685df9c2537ad5bc6240bf
javiertruquin <javiertruquin@gmail.com>
2022-10-19 17:27:28 -0300

    somos dkz

--- src/components/Celular/Nosotros/Banner.jsx
+++ src/components/Celular/Nosotros/Banner.jsx
@@ -1,41 +1,44 @@
 import React from "react";
 
 export default function Banner() {
-  return (
-    <div className="pb-5 color-azul-oscuro-fondo">
-      <div className="color-azul-oscuro-fondo">
-        <div className="fondo-banner-conocenos">
-          <div className="card-position d-flex justify-content-center align-items-end">
-            <div className=" titulo-conocenos d-flex flex-column justify-content-center ">
-              <div className="my-5 mx-4">
-                <h1 className="titulos-impacto px-2  peso-bold-italic text-white text-center mas-espaciado">
-                  +20 PROFESIONALES A TU DISPOSICIÓN
-                  <br />
-                </h1>
-              </div>
+    return (
+        <div className="pb-5 color-azul-oscuro-fondo">
+            <div className="color-azul-oscuro-fondo">
+                <div className="fondo-banner-conocenos">
+                    <div className="card-position d-flex justify-content-center align-items-end">
+                        <div className=" titulo-conocenos d-flex flex-column justify-content-center ">
+                            <div className="my-5 mx-4">
+                                <h1 className="titulos-impacto px-2  peso-bold-italic text-white text-center mas-espaciado">
+                                    +20 PROFESIONALES A TU DISPOSICIÓN
+                                    <br />
+                                </h1>
+                            </div>
+                        </div>
+                        <div className="card-home-conocenos color-violeta-fondo-banner-pie banner-pie-cel rounder-3 px-1">
+                            <div className="my-4 mx-3">
+                                <h1 className="peso-bold-italic text-white text-center">
+                                    Un gran equipo
+                                </h1>
+                                <p className="peso-italic tamaño-medio px-2 text-white text-center texto-sombra">
+                                    Somos un equipo de creativos, profesionales
+                                    publicitarios, acordes a las demandas y las
+                                    exigencias del cliente. Explotamos en
+                                    conjunto con la revolución digital de la
+                                    nueva era. Somos artistas avocados a nuestra
+                                    profesión, pasionales, entusiastas. Tenemos
+                                    la capacidad de formular, crear, ejecutar y
+                                    evaluar propuestas publicitarias dirigidas a
+                                    consumidores activos. Nuestra imaginación
+                                    nos lleva a lugares recónditos de nuestra
+                                    mente, buscando explotar cada
+                                    neurotransmisor para crear productos únicos
+                                    e inigualables.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="card-home-conocenos color-violeta-fondo-banner-pie rounder-3 px-1">
-              <div className="my-4 mx-3">
-                <h1 className="titulos-impacto peso-bold-italic text-white text-center">
-                  Somos un equipo
-                </h1>
-                <p className="peso-italic tamaño-medio px-2 text-white text-center texto-sombra">
-                  Somos un equipo de creativos, profesionales publicitarios,
-                  acordes a las demandas y las exigencias del cliente. 
-                  Explotamos en conjunto con la revolución digital de la nueva
-                  era. Somos artistas avocados a nuestra profesión, pasionales,
-                  entusiastas.
-                  Tenemos la capacidad de formular, crear, ejecutar y evaluar
-                  propuestas publicitarias dirigidas a consumidores activos.
-                  Nuestra imaginación nos lleva a lugares recónditos de
-                  nuestra mente, buscando explotar cada neurotransmisor para
-                  crear productos únicos e inigualables.
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
