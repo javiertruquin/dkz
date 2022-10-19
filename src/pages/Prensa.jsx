14522d47c7dbe9bac4a0d7ec54c591a48a3a8eb9
¨mago¨ <martingramajo08@gmail.com>
2022-10-19 18:33:38 -0300

    prensa terminado

--- src/pages/Prensa.jsx
+++ src/pages/Prensa.jsx
@@ -1,9 +1,4 @@
-import BotonContactanos from "../components/BotonContactanos";
-import BannerPrensa from "../components/Celular/Prensa/BannerPrensa";
-import PrensaEscritorio from "../components/Escritorio/Prensa/PrensaEscritorio";
-import { Radio } from "../components/Radio";
 import PrensaBanner from "../components/PrensaBanner";
-import TrabajosPrensa from "../components/TrabajosPrensa";
 import PrensaContenido from "../components/PrensaContenido";
 
 export default function Prensa() {
