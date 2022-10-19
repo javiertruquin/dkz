f63b12696d729ddbe8169c1b6adff727da8ee567
¨mago¨ <martingramajo08@gmail.com>
2022-10-19 17:44:41 -0300

    dg

--- src/components/DGraficoBanner.jsx
+++ src/components/DGraficoBanner.jsx
@@ -36,24 +36,40 @@ export default function DGraficoBanner() {
             <div className="titulo-audiovisual d-flex flex-column justify-content-center">
               <div className="my-5 mx-3">
                 <h1 className="titulos-impacto tamaño-mas-grande peso-bold-italic text-white text-center espaciado mt-5 titulo-producciones">
-                DISEÑO GRÁFICO
+                  DISEÑO GRÁFICO
                 </h1>
               </div>
             </div>
             <div className="banner-pie-cel d-flex justify-content-center color-violeta-fondo-banner-pie px-1 rounded-3">
               <div className="text-white">
                 <p className="peso-italic tamaño-medio px-2 py-4">
-                Tenemos una creencia, un concepto: podemos ser eróticos 
-              conceptualmente, sin la necesidad de ser pornos.  Por eso nos
-              arriesgamos, implementando desafíos en cada pieza que ejecutamos,
-              siendo disruptivos y marcando tendencia en el mercado.  ¿Cómo lo
-              hacemos? De acuerdo a cada proyecto, montamos unidades operativas
-              de trabajo, agregando profesionales de DKZ con amplia trayectoria
-              en nivel ejecutivo y creativo.
+                  Tenemos una creencia, un concepto: podemos ser eróticos
+                  conceptualmente, sin la necesidad de ser pornos. Por eso nos
+                  arriesgamos, implementando desafíos en cada pieza que
+                  ejecutamos, siendo disruptivos y marcando tendencia en el
+                  mercado. ¿Cómo lo hacemos? De acuerdo a cada proyecto,
+                  montamos unidades operativas de trabajo, agregando
+                  profesionales de DKZ con amplia trayectoria en nivel ejecutivo
+                  y creativo.
                 </p>
               </div>
             </div>
           </div>
+        </div>
+        <div className="d-flex justify-content-center pt-4 px-5">
+          <p className="tamaño-medio peso-italic text-white espaciado texto-sombra">
+            <span className="tamaño-grande peso-bold-italic text-white text-center espaciado texto-sombra">
+              Nuestros diferentes servicios de diseño son:
+            </span>
+            <br />
+            -Masivos: carteles, infografía, flyers, simbología y señalética,
+            folletos, etcétera. <br /> -Diseño web: maquetación y diseño final
+            de prototipos web. <br /> -Diseño de Identidad Visual o Branding:
+            desarrollamos manuales corporativos de marcas con diferentes
+            aplicaciones y envergaduras. <br /> -Diseños de productos
+            Multimedia. <br /> -Diseño Publicitario. <br /> -Diseño de
+            Packaging. <br /> -Diseño de Merchandising
+          </p>
         </div>
       </div>
     </div>
