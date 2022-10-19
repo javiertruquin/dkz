5d19c781945f4dde6914756321859b35a0cbeed8
¨mago¨ <martingramajo08@gmail.com>
2022-10-19 17:46:08 -0300

    dg text

--- src/components/DGTexto.jsx
+++ src/components/DGTexto.jsx
@@ -5,14 +5,14 @@ export default function DGTexto() {
     <div>
       <div className="d-none d-md-block">
         <div className="d-flex justify-content-center">
-          <div>
+          <div className="container">
             <p className="tamaño-medio peso-italic text-white text-center espaciado texto-sombra">
               Tenemos una creencia, un concepto: podemos ser eróticos
-              conceptualmente, sin la necesidad de ser pornos. <br /> Por eso
+              conceptualmente, sin la necesidad de ser pornos. Por eso
               nos arriesgamos, implementando desafíos en cada pieza que
-              ejecutamos, siendo disruptivos <br /> y marcando tendencia en el
+              ejecutamos, siendo disruptivos y marcando tendencia en el
               mercado. ¿Cómo lo hacemos? De acuerdo a cada proyecto, montamos
-              unidades operativas de trabajo, <br /> agregando profesionales de
+              unidades operativas de trabajo, agregando profesionales de
               DKZ con amplia trayectoria en nivel ejecutivo y creativo.
             </p>
           </div>
